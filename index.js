3a8d32e601962757d5074d09801aa21dcc917f32
Chris Hayes <chris@moderncognition.com>
2016-09-28 15:15:00 -0400

    Shim emojione and import necessary files

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-emojione'
+  name: 'ember-cli-emojione',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/emojione/assets/css/emojione.css');
+    app.import(app.bowerDirectory + '/emojione/lib/js/emojione.js');
+    app.import('vendor/emojione.js', {
+      exports: {
+        emojione: ['default']
+      }
+    });
+  }
 };
